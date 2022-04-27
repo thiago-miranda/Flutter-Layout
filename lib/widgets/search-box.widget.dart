d0afcc6f5fe439f09f9c238828e175efa6c0a087
thiago-miranda <thiago.miranda@hotmail.com>
2022-04-27 00:09:20 -0300

    Animate Search-box - Item-Drawer

--- lib/widgets/search-box.widget.dart
+++ lib/widgets/search-box.widget.dart
@@ -9,7 +9,8 @@ class SearchBox extends StatefulWidget {
 
 class _SearchBoxState extends State<SearchBox>
     with SingleTickerProviderStateMixin {
-  AnimationController?  _controller;
+  AnimationController? _controller;
+  bool menuOpened = false;
 
   @override
   void initState() {
@@ -30,42 +31,21 @@ class _SearchBoxState extends State<SearchBox>
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      padding: const EdgeInsets.only(left: 20),
-      decoration: const BoxDecoration(
-        color: Color.fromRGBO(0, 0, 0, 0.1),
-        borderRadius: BorderRadius.all(
-          Radius.circular(
-            128,
-          ),
-        ),
-      ),
-      child: Row(
-        children: [
-          const Icon(Icons.search),
-          Container(
-            width: 300,
-            padding: const EdgeInsets.only(left: 10),
-            child: TextFormField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                labelText: "Search...",
-                labelStyle: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontWeight: FontWeight.w300,
-                  fontSize: 16,
-                ),
-              ),
-              style: TextStyle(
-                fontSize: 20,
-                color: Theme.of(context).primaryColor,
-              ),
-            ),
-          ),
-        ],
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text("Search"),
+        TextButton(
+            onPressed: () {
+              !menuOpened ? _controller!.forward() : _controller!.reverse();
+              menuOpened = !menuOpened;
+            },
+            child: AnimatedIcon(
+              icon: AnimatedIcons.menu_close,
+              progress: _controller!,
+              semanticLabel: "Open Menu",
+            ))
+      ],
     );
   }
 }
